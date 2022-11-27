763b4caa471ca7cab446f4b6b35e8aa26b8d8c11
optor666 <optor@qq.com>
2022-11-27 17:23:06 +0800

    Add code/c07_7-7_2.asm.

---
+++ code/c07_7-7_1.asm
@@ -0,0 +1,58 @@
+; nasm -f bin c07_7-7_1.asm -l c07_7-7_1.lst -o c07_7-7_1.bin
+; dd if=c07_7-7_1.bin of='/Users/clearbug/VirtualBox VMs/LEARN-ASM/LEARN-ASM.vhd' bs=512 count=1 conv=notrunc
+	jmp near start
+
+message db '1+2+3+...+100='
+
+start:
+	mov ax,0x7c0
+	mov ds,ax
+
+	mov ax,0xb800
+	mov es,ax
+
+	mov si,message
+	mov di,0
+	mov cx,start-message
+@g:
+	mov al,[si]
+	mov [es:di],al
+	inc di
+	mov byte [es:di],0x07
+	inc di
+	inc si
+	loop @g
+
+	xor ax,ax
+	mov cx,100
+@f:
+	add ax,cx
+	loop @f
+
+	xor cx,cx
+	mov ss,cx
+	mov sp,cx
+
+	mov bx,10
+	xor cx,cx
+@d:
+	inc cx
+	xor dx,dx
+	div bx
+	or dl,0x30
+	push dx
+	cmp ax,0
+	jne @d
+
+@a:
+	pop dx
+	mov [es:di],dl
+	inc di
+	mov byte [es:di],0x07
+	inc di
+	loop @a
+
+	jmp near $
+
+times 510-($-$$) db 0
+				 db 0x55,0xaa
